93bc8a37b218208911f2d10e36ea8e3879a11879
annavancity <anna.designers@yahoo.com>
2024-02-20 12:03:37 -0800

    navbar burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,42 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../src/images/logo.svg";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    // Toggle the overflow style on body to prevent/allow scrolling
+    if (!isOpen) {
+      document.body.style.overflow = "hidden"; // Prevent scrolling
+      document.body.style.backgroundColor = "var(--color-white)";
+    } else {
+      document.body.style.overflow = ""; // Allow scrolling
+      document.body.style.backgroundColor = "var(--color-background)";
+    }
+  };
+
   return (
-    <nav className="navbar">
-      <div className="container block nav">
-        <div className="links-to-main">
-          <div className="logo">
-            <Link to="https://www.fastepp.com/">
-              <img src={logo} alt="logo" />
-            </Link>
-          </div>
+    <nav className="navbar container">
+      <div className="logo">
+        <Link to="https://www.fastepp.com/">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className={`menu-icon ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className={`links-to-main ${isOpen ? "open" : ""}`}>
+        <div className="concept-lab-menu">
           <ul>
             <li>
               <Link to="https://www.fastepp.com/concept-lab/">CONCEPT LAB</Link>
             </li>
           </ul>
         </div>
-
         <div className="main-menu">
           <ul>
             <li>
